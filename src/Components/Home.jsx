a66167ec41071b9443d48c17e640f91fe3c386d4
Howard-Zhou <howard12345678987654321@hotmail.com>
2016-10-24 00:16:21 -0700

    link added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
+import { Link } from 'react-router';
 import '../css/App.css';
 
 class Home extends Component {
@@ -22,10 +23,9 @@ class Home extends Component {
     return (
       <div style={{display:'flex',flexDirection:'column', alignItems:'center', justifyContent:'center'}}>
         <div style={{display:'flex',flexDirection:'row',marginLeft:window.innerWidth/1.5}}>
-          <h3 style={{marginRight:50}}>courses</h3>
-          <h3 style={{marginRight:50}}>posts</h3>
-          <h3 style={{marginRight:50}}>team</h3>
-          <h3 style={{marginRight:50}}>about</h3>
+          <li style={{marginRight:50}}><Link to="/courses">courses</Link></li>
+          <li style={{marginRight:50}}><Link to="/team">team</Link></li>
+          <li ><Link to="/about">about</Link></li>
         </div>
         <div style={{display:'flex',flexDirection:'column', alignItems:'center', justifyContent:'center',marginTop:80}}>
           <img src={logo} className="App-logo" alt="logo" />
@@ -35,8 +35,8 @@ class Home extends Component {
           Home Page
         </p>
         <div style={{display:'flex', flexDirection:'column', alignItems:'center',justifyContent:"center"}}>
-          <input style={{margin:20}} type="text" value={this.state.userName} onChange={this.setUserName.bind(this)}/>
-          <input style={{margin:20}} type="text" value={this.state.password} onChange={this.setPassword.bind(this)}/>
+          <input style={{margin:20,width:300,height:45}} type="text" value={this.state.userName} onChange={this.setUserName.bind(this)}/>
+          <input style={{margin:20,width:300,height:45}} type="text" value={this.state.password} onChange={this.setPassword.bind(this)}/>
         </div>
         <button>click</button>
       </div>
