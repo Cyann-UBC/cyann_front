99a237c95de02a8514b2cf69b6eb84202f26c4da
chuxi <chuxitina@hotmail.com>
2016-11-09 21:00:58 -0800

    add the rout to profssor webpage in router

--- src/router.jsx
+++ src/router.jsx
@@ -6,13 +6,16 @@ import Home from './Components/Home';
 import Courses from './Components/Courses';
 import About from './Components/About';
 import Team from './Components/Team';
+import prof from './Components/prof';
 
 const routes = (
   <Router history={browserHistory}>
       <Route path="/" component={Home}></Route>
       <Route path='about' component={About}></Route>
       <Route path='team' component={Team}></Route>
-      <Route path='courses' component={Courses}></Route>
+      <Route path='courses' component={Courses}></Route> 
+      <Route path='prof' component={prof}></Route>
+    
   </Router>
 );
 
