1d21cc9b7df7ade265822dcd87a23e59df0bd668
joshuaqiao28 <joshuaqiao28@users.noreply.github.com>
2016-11-16 18:08:50 -0800

    update router links with CreateUser page

--- src/router.jsx
+++ src/router.jsx
@@ -7,15 +7,17 @@ import Courses from './Components/Courses';
 import About from './Components/About';
 import Team from './Components/Team';
 import prof from './Components/prof';
+import CreateUser from './Components/CreateUser';
 
 const routes = (
   <Router history={browserHistory}>
       <Route path="/" component={Home}></Route>
       <Route path='about' component={About}></Route>
       <Route path='team' component={Team}></Route>
-      <Route path='courses' component={Courses}></Route> 
+      <Route path='courses' component={Courses}></Route>
       <Route path='prof' component={prof}></Route>
-    
+      <Route path='CreateUser' component={CreateUser}></Route>
+
   </Router>
 );
 
