5bc68a3ce21d12dd82652ceaf5c58e97a866e561
joshuaqiao28 <joshuaqiao28@users.noreply.github.com>
2016-11-21 14:56:57 -0800

    home page

--- src/router.jsx
+++ src/router.jsx
@@ -7,17 +7,16 @@ import Courses from './Components/Courses';
 import About from './Components/About';
 import Team from './Components/Team';
 import prof from './Components/prof';
-import CreateUser from './Components/CreateUser';
 
 const routes = (
   <Router history={browserHistory}>
-      <Route path="/" component={Home}></Route>
+      <Route path='/' component={Home}>
+        {/* <Route path='/course/:value' component={Courses} /> */}
+      </Route>
+      <Route path='courses' component={Courses}/>
       <Route path='about' component={About}></Route>
       <Route path='team' component={Team}></Route>
-      <Route path='courses' component={Courses}></Route>
       <Route path='prof' component={prof}></Route>
-      <Route path='CreateUser' component={CreateUser}></Route>
-
   </Router>
 );
 
